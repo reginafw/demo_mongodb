a2ef44eb330f82b11a3d6527f1e3c60d959d2f4e
Regina Weinreb <rweinreb@umail.ucsb.edu>
2018-10-11 19:52:36 -0700

    worked on it

--- src/main/java/edu/ucsb/cs56/pconrad/SparkDemo01.java
+++ src/main/java/edu/ucsb/cs56/pconrad/SparkDemo01.java
@@ -26,7 +26,12 @@ public class SparkDemo01 {
 		spark.Spark.get("/nihao", (req, res) -> "<b>Ni Hao</b>\n");
 
 		spark.Spark.get("/hola", (req, res) -> "<b>¡Hola!</b>\n");
-		spark.Spark.get("/", (req, res) -> "<h1>Yo</h1>\n");
+		
+		String html="<h1><a href='/hello'>Hello</a> World!</h1>\n" + "<p>This web app is powered by \n" + "<a href='https://github.com/reginafw/sparkjava-01'>this github repo</a></p>\n";
+
+		spark.Spark.get("/", (req, res) -> html);
+
+		spark.Spark.get("/hello", (req, res) -> "<p><b>Hello, World!</b>  You just clicked the first link on my web app.</p>");
 
 	}
 	
